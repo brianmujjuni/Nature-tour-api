9c2e68d925cff66ddc8288a5b0cfb5737fc85241
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-05 16:42:18 +0300

    connected to mongodb

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
+const DB = process.env.DATABASE_URI.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // console.log(con.connections);
+    console.log('data base connection successful');
+  });
 //start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
