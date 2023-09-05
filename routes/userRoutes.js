8c3e16aa4aafde0e1e1b7ffa43292552a0dda459
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-05 12:31:54 +0300

    added user controller and refactored user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,41 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: "success",
-        msg: "Route under development"
-    })
-}
-
-const createUser = (req, res) => {
-    res.status(500).json({
-        status: "success",
-        msg: "Route under development"
-    })
-}
-
-const getUser = (req, res) => {
-    res.status(500).json({
-        status: "success",
-        msg: "Route under development"
-    })
-}
-
-const updateUser = (req, res) => {
-    res.status(500).json({
-        status: "success",
-        msg: "Route under development"
-    })
-}
-
-const deleteUser = (req, res) => {
-    res.status(500).json({
-        status: "success",
-        msg: "Route under development"
-    })
-}
-
+const {getAllUsers,createUser,getUser,updateUser,deleteUser} = require('../controllers/userController')
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
