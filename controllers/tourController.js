27fc682792c324d30aec990bb94eedb9b3054d1a
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-05 18:42:46 +0300

    added crud for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,17 +1,40 @@
 const Tour = require('../models/tourModel');
 
 //function to get all tours
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: false,
+      msg: error,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {},
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'succes',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'Failed to create tour',
+      err: error,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -32,7 +55,24 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {};
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'Failed to create tour',
+      err: error,
+    });
+  }
+};
 
 exports.deleteTour = (req, res) => {
   res.status(204).json({
