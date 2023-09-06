c0d67b58a14d61397fdaa5a7436e218cc093577b
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-06 11:13:04 +0300

    add filtering to get all tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,14 @@ const Tour = require('../models/tourModel');
 //function to get all tours
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //Build the query
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+    //excute the query
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
