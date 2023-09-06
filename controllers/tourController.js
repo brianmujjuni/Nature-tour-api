826eba05b2b1ca86a4c99a3dd6e85f6fd805388c
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-06 11:53:53 +0300

    add default sort in get all tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -19,6 +19,8 @@ exports.getAllTours = async (req, res) => {
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
     }
     //excute the query
     const tours = await query;
