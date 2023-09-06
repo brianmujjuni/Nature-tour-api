94d6b8b49483d79fce8b6bd0f5e1cedd2e5cb428
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-06 11:28:09 +0300

    implement advanced sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,12 +3,17 @@ const Tour = require('../models/tourModel');
 //function to get all tours
 exports.getAllTours = async (req, res) => {
   try {
+    //1)Filtering
     //Build the query
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    //2)Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
     //excute the query
     const tours = await query;
 
