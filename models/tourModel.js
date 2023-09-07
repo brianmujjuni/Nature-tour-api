3a8b974cfa5573cdbe2f33b41cff23e0967da7c9
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-07 13:00:03 +0300

    added other mongo validators to tours model

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: true,
       unique: true,
       trim: true,
+      maxlength: [40, 'The Tour name must have less or equal to 40 characters'],
+      minlength: [10, 'The tour should exceed 10 characters'],
     },
     duration: {
       type: Number,
@@ -20,10 +22,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -52,6 +60,10 @@ const tourSchema = new mongoose.Schema(
     slug: {
       type: String,
     },
+    secreteTour: {
+      type: Boolean,
+      default: false,
+    },
     createdAt: {
       type: Date,
       default: Date.now(),
@@ -70,6 +82,18 @@ tourSchema.virtual('durationWeeks').get(function () {
 //Document middle ware runs beafore save
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+//agregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secreteTour: { $ne: true } } });
+  next();
+});
+
+//query middleware
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secreteTour: { $ne: true } });
   next();
 });
 
