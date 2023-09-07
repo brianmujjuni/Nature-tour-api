43a445e12604114a811ef86742b34a9af55710e0
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-07 11:42:54 +0300

    finshing up data aggregation pipline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -121,7 +121,11 @@ exports.getTourStats = async (req, res) => {
           maxPrice: { $max: '$price' },
         },
       },
-      
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
     ]);
     res.status(200).json({
       status: 'success',
@@ -131,7 +135,59 @@ exports.getTourStats = async (req, res) => {
     });
   } catch (error) {
     res.status(400).json({
-      status: 'Failed to create tour',
+      status: 'Failed',
+      err: error,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      { $unwind: '$startDates' },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numToursStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: {
+          numToursStarts: -1,
+        },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'Failed',
       err: error,
     });
   }
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,10 +7,12 @@ const {
   updateTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
