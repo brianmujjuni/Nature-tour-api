41e9e44f6bc3bca645221aa232ec96fd02998ea1
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-11 17:53:36 +0300

    added signup user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,34 @@
-
 exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: "success",
-        msg: "Route under development"
-    })
-}
+  res.status(500).json({
+    status: 'success',
+    msg: 'Route under development',
+  });
+};
 
 exports.createUser = (req, res) => {
-    res.status(500).json({
-        status: "success",
-        msg: "Route under development"
-    })
-}
+  res.status(500).json({
+    status: 'success',
+    msg: 'Route under development',
+  });
+};
 
 exports.getUser = (req, res) => {
-    res.status(500).json({
-        status: "success",
-        msg: "Route under development"
-    })
-}
+  res.status(500).json({
+    status: 'success',
+    msg: 'Route under development',
+  });
+};
 
 exports.updateUser = (req, res) => {
-    res.status(500).json({
-        status: "success",
-        msg: "Route under development"
-    })
-}
+  res.status(500).json({
+    status: 'success',
+    msg: 'Route under development',
+  });
+};
 
 exports.deleteUser = (req, res) => {
-    res.status(500).json({
-        status: "success",
-        msg: "Route under development"
-    })
-}
+  res.status(500).json({
+    status: 'success',
+    msg: 'Route under development',
+  });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,19 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const {getAllUsers,createUser,getUser,updateUser,deleteUser} = require('../controllers/userController')
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController');
+const { signup } = require('../controllers/authController');
+//auth routes
+router.route('/signup').post(signup);
 
-router.route('/').get(getAllUsers).post(createUser)
-router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
+//user routes
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
-module.exports = router
+module.exports = router;
