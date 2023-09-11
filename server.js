101a511e94bf8b7403ed3b042a937b3066c0c741
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-11 16:59:15 +0300

    handle unhandled rejections

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ mongoose
 
 //start the server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}......`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
