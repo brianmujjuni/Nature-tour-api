878593f82414e39bd782270f0820cf6113fdf16b
brianmujjuni <brianmujjuni@engineer.com>
2023-09-11 09:53:51 +0300

    error hanling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,32 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
+const sendErrorDev = (err,res)=>{
+    res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+const sendErrorPro =(err,res)={
+  //operational ,trusted error send message to client
+  if(err.isOperational){
+     res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+   }else{
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wron'
+    })
+   }
+ 
+}
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  if(process.env.NODE_ENV === 'development'){
+  sendErrorDev(err,res);
+  }else if(process.env.NODE_ENV === 'production')
+  sendErrorPro(err,res);
 };
