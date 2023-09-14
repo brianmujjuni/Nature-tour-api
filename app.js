f2617b21f2fd035c852f5a8e03b5ade25b62072e
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-14 12:11:44 +0300

    impelement data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const AppError = require('./utills/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -13,6 +16,12 @@ const app = express();
 app.use(helmet());
 //Body parser,reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against injections
+app.use(mongoSanitize());
+//Data sanitizaation against xss
+app.use(xss());
+
 //developemnt logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
