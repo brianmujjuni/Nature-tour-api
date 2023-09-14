dc225e8c16d543016ffd6d7ff01649c31d79545f
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-14 10:22:19 +0300

    send http cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,19 @@ const signToken = (id) =>
 //refactor token generation
 const createSentToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 100,
+    ),
+
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
@@ -156,5 +169,3 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
   createSentToken(user, 201, res);
 });
-
-
