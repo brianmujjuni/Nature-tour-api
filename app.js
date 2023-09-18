4438879ccf2cdb4a0b6e5fb5fcfb12a830a1d119
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-18 12:21:04 +0300

    add review controller and model

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 //Set security HTTP headers
@@ -62,6 +63,7 @@ app.use((req, res, next) => {
 //mounting routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 //show error of not existing route
 app.all('*', (req, res, next) => {
