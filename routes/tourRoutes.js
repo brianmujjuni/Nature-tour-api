59d2ea965885957582bb49d484da3610e6287a3c
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-20 10:04:03 +0300

    prtotect tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -17,13 +17,18 @@ const router = express.Router();
 router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/tour-stats').get(getTourStats);
-router.route('/monthly-plan/:year').get(getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(protect, restrictTo('admin', 'leade-guide'), getMonthlyPlan);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
-router.route('/').get(protect, getAllTours).post(createTour);
+router
+  .route('/')
+  .get(getAllTours)
+  .post(protect, restrictTo('admin', 'leade-guide'), createTour);
 router
   .route('/:id')
   .get(getTour)
-  .patch(updateTour)
+  .patch(protect, restrictTo('admin', 'leade-guide'), updateTour)
   .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
