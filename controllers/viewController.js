433d4cbc2b69540fd9a347c6d57203cd66c200a3
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-22 11:30:20 +0300

    layout overview and fetch tours initial1

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,12 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'Overview' });
-};
+const Tour = require('../models/tourModel');
+
+const catchAsync = require('../utills/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
+  res.status(200).render('overview', { tours });
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', { title: 'Tour' });
