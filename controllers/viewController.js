87ef412812b7ebf5a359a91517696c105a72909c
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-22 16:51:40 +0300

    building tour page part2

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -8,6 +8,11 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   res.status(200).render('overview', { tours });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', { title: 'Tour' });
-};
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  res.status(200).render('tour', { tour });
+});
