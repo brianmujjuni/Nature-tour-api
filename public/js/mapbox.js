73a21854be118349e290a3b6a09db1f4740da07d
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-23 12:12:59 +0300

    allow and map box to display in the review router

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -2,3 +2,11 @@
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYnJpYW5tdWpqdW5pIiwiYSI6ImNsbXZzMWN5OTBxbjYybHZ6eWx6MnJqZTcifQ.WMCkca6iYkEhKm7uPsl0Cw';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+});
