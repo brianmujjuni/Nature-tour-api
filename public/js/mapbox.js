0a8b4eb250b3541e1266dfda361012c1807d4e7e
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-23 15:49:01 +0300

    compleeted map intergrated

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -9,4 +9,38 @@ mapboxgl.accessToken =
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
+  scrollZoom: false
+  //   center: [-116.214531, 51.417611],
+  //   zoom: 4,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  //Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+  //Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+  new mapboxgl.Popup({ offset: 30 })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+  //Add pop
+
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
 });
