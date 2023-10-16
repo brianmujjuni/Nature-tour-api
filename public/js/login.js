b97c4c8b9ec1c4bf7750adc0f248c8c60e85e766
Mujjuni Brian <brianmujjuni@engineer.com>
2023-10-16 10:52:37 +0300

    remove passed image from login function

--- public/js/login.js
+++ public/js/login.js
@@ -21,5 +21,5 @@ document.querySelector('.form').addEventListener('submit', (e) => {
   e.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
-  login({ email, password });
+  login(email, password);
 });
